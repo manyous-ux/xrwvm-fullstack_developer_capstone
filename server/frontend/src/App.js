c4d23884b7efd6bce30eb1804245a14b7cc34134
manyous <mans1457@gmail.com>
2024-03-21 05:52:45 -0400

    adding User Managment Changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
 import LoginPanel from "./components/Login/Login"
+import RegistrationPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/login" element={<LoginPanel />}/>
+      <Route path="/register" element={<RegistrationPanel />}/>
     </Routes>
+    
   );
 }
 export default App;
